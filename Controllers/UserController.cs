a119e44e3ca14ebcf1fe737de757f1b37dff751c
Evelyn Anyebe <anyebeevelyn@gmail.com>
2021-09-05 03:59:55 +0100

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CSharpApi.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSharpApi.Data;
+using CSharpApi.Commons;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharpApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private WomenTechstersDBContext _dbContext;
+
+        public UserController(WomenTechstersDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("GetUsers")]
+        public IActionResult Get()
+        {
+            try
+            {
+                var customers = _dbContext.customer.ToList();
+
+                if(customers.Count == 0)
+                {
+                    return StatusCode(404, "No customer found");
+                }
+                return Ok(customers);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "An error occured "+e.Message);
+            }
+            
+        }
+
+        [HttpPost("CreateUser")]
+        public IActionResult Create([FromBody] CustomerRequest customerRequest)
+        {
+            try
+            {
+                Customer customer = new Customer();
+                customer.FirstName = customerRequest.FirstName;
+                customer.Email = customerRequest.Email;
+                customer.LastName = customerRequest.LastName;
+                customer.PhoneNumber = customerRequest.PhoneNumber;
+                customer.CustomerPassword = EncryptionUtilities.EncryptString(customerRequest.CustomerPassword);
+
+                _dbContext.customer.Add(customer);
+                _dbContext.SaveChanges();
+
+                var customers = _dbContext.customer.ToList();
+                return Ok(customers);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "An error occured " + e.Message);
+            }
+
+        }
+
+        [HttpPut("UpdateUser")]
+        public IActionResult Update([FromBody] CustomerRequest customerRequest)
+        {
+            try
+            {
+                var customer = _dbContext.customer.FirstOrDefault(x=>x.CustomerId==customerRequest.CustomerId);
+                if (customer == null)
+                {
+                    return StatusCode(404, "No customer found");
+                }
+
+                customer.FirstName = customerRequest.FirstName;
+                customer.Email = customerRequest.Email;
+                customer.LastName = customerRequest.LastName;
+                customer.PhoneNumber = customerRequest.PhoneNumber;
+                customer.CustomerPassword = EncryptionUtilities.EncryptString(customerRequest.CustomerPassword);
+
+                _dbContext.Entry(customer).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+
+                var customers = _dbContext.customer.ToList();
+                return Ok(customers);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "An error occured " + e.Message);
+            }
+
+        }
+
+
+        [HttpDelete("DeleteUser/{Id}")]
+        public IActionResult Delete([FromRoute] int Id)
+        {
+            try
+            {
+                var customer = _dbContext.customer.FirstOrDefault(x => x.CustomerId == Id);
+                if (customer == null)
+                {
+                    return StatusCode(404, "No customer found");
+                }
+
+                
+                _dbContext.Entry(customer).State = EntityState.Deleted;
+                _dbContext.SaveChanges();
+
+                var customers = _dbContext.customer.ToList();
+                return Ok(customers);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "An error occured " + e.Message);
+            }
+        }
+    }
+}
---
+++ Data/WomenTechstersDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpApi.Data
+{
+    public class WomenTechstersDBContext :DbContext
+    {
+        public WomenTechstersDBContext(DbContextOptions<WomenTechstersDBContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Customer> customer { get; set; }
+    }
+}
